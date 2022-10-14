d548b73a0032d72fb0b9dd19d72ee7390f801632
Samaze123456 <he202053@students.ephec.be>
2022-10-14 22:40:22 +0200

    db

--- Main/main.py
+++ Main/main.py
@@ -6,10 +6,7 @@ def main():
     dbConnection = sqlite3.connect("./Db/bambooConcess.db")
     dbCursor = dbConnection.cursor()
 
-    dbCursor.execute("select idCar, firstNameCusto \
-    from deal \
-    natural join Customer \
-    where idCusto = 1")
+    dbCursor.execute("select idCar, firstNameCusto from deal natural join Customer where idCusto = 1")
     array = dbCursor.fetchall()
     for e in array:
         print(e)
