c94ab8bdfe5380654974af64a9c47ee0e36f53d2
Samaze123456 <he202053@students.ephec.be>
2022-10-14 16:51:45 +0200

    First Select from db

--- Main/main.py
+++ Main/main.py
@@ -1,8 +1,15 @@
 from Class.Car import Car
+import sqlite3
 
 def main():
-    stock = ["test"]
-    print(stock)
+
+    dbConnection = sqlite3.connect("./Db/bambooConcess.db")
+    dbCursor = dbConnection.cursor()
+    dbCursor.execute("SELECT * from Motor")
+    array = dbCursor.fetchall()
+    for e in array:
+        print(e)
+    dbCursor.close()
     
 if __name__ == "__main__":
     main()
