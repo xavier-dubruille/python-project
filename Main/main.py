17de63fc96be94b23eaf16e72ee2356bc9f569e4
Samaze123456 <he202053@students.ephec.be>
2022-10-15 14:59:07 +0200

    frame

--- Main/main.py
+++ Main/main.py
@@ -12,7 +12,7 @@ def ExecuteQuery(query, frameResults):
     count = 0
     for e in cursor.fetchall():
         arrayLabelQuery.append(tk.Label(frameResults, text = e))
-        arrayLabelQuery[count].grid()
+        arrayLabelQuery[count].pack()
         count += 1
     cursor.close()
 
@@ -21,18 +21,33 @@ def main():
 
     window = tk.Tk()
     window.title("Bamboo Concess")
-    window.geometry("{}x{}+{}+{}".format(500, 500, 50,50))
+    window.attributes('-fullscreen', True)
 
-    frameButtonQuit = tk.Frame(window)
-    frameButtonQuit.grid()
+    window.columnconfigure(0, weight = 2)
+    window.columnconfigure(1, weight = 3)
+    window.columnconfigure(2, weight = 1)
+    window.columnconfigure(3, weight = 1)
+
+    for i in range(6):
+        window.rowconfigure(i, weight = 1)
+    window.rowconfigure(7, weight = 2)
+
+    frameReservation = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
+    frameReservation.grid(column = 0, row = 0, rowspan = 3, sticky = "wesn")
+
+    frameStock = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
+    frameStock.grid(column = 1, row = 1, rowspan=2, sticky="wesn")
+
+    frameStockAjout = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
+    frameStockAjout.grid(column = 2, row = 2, sticky = "wesn")
+
+    frameButtonQuit = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
+    frameButtonQuit.grid(column = 3, row = 0, sticky = "wesn")
 
     buttonQuit = tk.Button(frameButtonQuit, text = "Quit", command = window.destroy)
     buttonQuit.pack()
 
-
-    # dbCursor.execute("select idCar, firstNameCusto from deal natural join Customer where idCusto = 1")
-    buttonQuery = tk.Button(window, text = "query", command = lambda : ExecuteQuery('select idDeal, substr(firstNameCusto, 1, 1) || \'.  \' || lastNameCusto as nameCusto,  idCar, nameBrand, nameMotor, nameType from Deal natural join Car natural join Brand natural join Motor natural join Type natural join customer where firstNameCusto = "Samuel"', window))
-    buttonQuery.grid()
+    ExecuteQuery("select prixCar as Prix, nameBrand as Brand, nameMotor as Motor, nameType as Type, stockDateCar as DateStock, techControlDateCar as ControlDate, promoCar as Promotion from car NATURAL join Brand NATURAL join Motor NATURAL join Type", frameStock)
 
     window.mainloop()
 
