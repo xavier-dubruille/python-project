94332a21b87a5799bcf8db2f2bbd7422197c05a2
Samaze123456 <he202053@students.ephec.be>
2022-10-15 15:37:01 +0200

    fzefez

--- Main/main.py
+++ Main/main.py
@@ -1,6 +1,7 @@
 from Class.Car import Car
 import sqlite3 as sql
 import tkinter as tk
+import tkinter.font as font
 
 def ExecuteQuery(query, frameResults): 
     dbConnection = sql.connect("./Db/bambooConcess.db")
@@ -15,6 +16,22 @@ def ExecuteQuery(query, frameResults):
         arrayLabelQuery[count].pack()
         count += 1
     cursor.close()
+
+def ShowStock(frameResults):
+    ExecuteQuery("select prixCar as Prix, nameBrand as Brand, nameMotor as Motor, nameType as Type, \
+        stockDateCar as DateStock, techControlDateCar as ControlDate, promoCar as Promotion from car \
+        NATURAL join Brand NATURAL join Motor NATURAL join Type", frameResults)
+
+def ShowHistory(frameResults):
+    ExecuteQuery("select prixCar as Prix, nameBrand as Brand, nameMotor as Motor, nameType as Type, \
+        stockDateCar as DateStock, techControlDateCar as ControlDate, promoCar as Promotion from car \
+        NATURAL join Brand NATURAL join Motor NATURAL join Type", frameResults)
+
+def MakeReservation(): 
+    pass
+
+def MakeDeal(): 
+    pass
 
 
 def main():
@@ -32,22 +49,44 @@ def main():
         window.rowconfigure(i, weight = 1)
     window.rowconfigure(7, weight = 2)
 
-    frameReservation = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
-    frameReservation.grid(column = 0, row = 0, rowspan = 3, sticky = "wesn")
+    frameButtons = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
+    frameButtons.grid(column = 0, row = 1, rowspan = 5, sticky = "wesn")
 
-    frameStock = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
-    frameStock.grid(column = 1, row = 1, rowspan=2, sticky="wesn")
+    frameTitle = tk.Frame(window, highlightthickness=2, highlightbackground = "yellow")
+    frameTitle.grid(column = 1, row = 0, sticky = "wesn")
 
-    frameStockAjout = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
-    frameStockAjout.grid(column = 2, row = 2, sticky = "wesn")
+    frameSort = tk.Frame(window, highlightthickness = 2, highlightbackground = "blue")
+    frameSort.grid(column = 1, row = 1, sticky = "wesn")
 
-    frameButtonQuit = tk.Frame(window, highlightthickness=2, highlightbackground = "black")
-    frameButtonQuit.grid(column = 3, row = 0, sticky = "wesn")
+    frameDisplay = tk.Frame(window, highlightthickness=2, highlightbackground = "pink")
+    frameDisplay.grid(column = 1, row = 2, rowspan=6, sticky="wesn")
 
-    buttonQuit = tk.Button(frameButtonQuit, text = "Exit", command = window.destroy)
-    buttonQuit.pack()
+    frameOnClick = tk.Frame(window, highlightthickness=2, highlightbackground = "purple")
+    frameOnClick.grid(column = 2, row = 1, rowspan = 7, columnspan = 2, sticky ="wesn")
 
-    ExecuteQuery("select prixCar as Prix, nameBrand as Brand, nameMotor as Motor, nameType as Type, stockDateCar as DateStock, techControlDateCar as ControlDate, promoCar as Promotion from car NATURAL join Brand NATURAL join Motor NATURAL join Type", frameStock)
+    frameExit = tk.Frame(window)
+    frameExit.grid(column = 3, row = 0, sticky = "wesn")
+
+    buttonStock = tk.Button(frameButtons, text = "Display Stock", command = lambda : ShowStock(frameDisplay))
+    buttonStock.pack()
+
+    buttonHistory = tk.Button(frameButtons, text = "Display History", command = lambda : ShowHistory(frameDisplay))
+    buttonHistory.pack()
+
+    buttonReservation = tk.Button(frameButtons, text = "Make a reservation", command = MakeReservation)
+    buttonReservation.pack()
+
+    buttonDeal = tk.Button(frameButtons, text = "Make a deal", command = MakeDeal)
+    buttonDeal.pack()
+
+    buttonAdd
+
+    buttonExit = tk.Button(frameExit, text = "Exit", command = window.destroy, relief='raised', font=font.Font(family='Helvetica', size=15, weight='bold'))
+    buttonExit.pack()
+
+    ExecuteQuery("select prixCar as Prix, nameBrand as Brand, nameMotor as Motor, nameType as Type, \
+        stockDateCar as DateStock, techControlDateCar as ControlDate, promoCar as Promotion from car \
+        NATURAL join Brand NATURAL join Motor NATURAL join Type", frameDisplay)
 
     window.mainloop()
 
