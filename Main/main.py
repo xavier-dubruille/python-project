f2952025ebd0d6be2096241b9523706bfef1e628
Samaze123456 <he202053@students.ephec.be>
2022-10-15 13:47:49 +0200

    dezed

--- Main/main.py
+++ Main/main.py
@@ -2,32 +2,35 @@ from Class.Car import Car
 import sqlite3 as sql
 import tkinter as tk
 
+def ExecuteQuery(query, frameResults): 
+    dbConnection = sql.connect("./Db/bambooConcess.db")
+    cursor = dbConnection.cursor()
+
+    cursor.execute(query) 
+
+    arrayLabelQuery = []
+    count = 0
+    for e in cursor.fetchall():
+        arrayLabelQuery.append(tk.Label(frameResults, text = e))
+        arrayLabelQuery[count].grid()
+        count += 1
+    cursor.close()
+
+
 def main():
 
     window = tk.Tk()
     window.title("Bamboo Concess")
     window.geometry("{}x{}+{}+{}".format(500, 500, 50,50))
 
-    frameButtonQuit = tk.Frame(window)
+    frameButtonQuit = tk.Frame(window, )
     frameButtonQuit.grid()
 
-    buttonQuit = tk.Button(frameButtonQuit, text = "Quit", command = lambda : window.destroy())
+    buttonQuit = tk.Button(frameButtonQuit, text = "Quit", command = window.destroy)
     buttonQuit.pack()
 
-    dbConnection = sql.connect("./Db/bambooConcess.db")
-    dbCursor = dbConnection.cursor()
-
-    dbCursor.execute("select idCar, firstNameCusto from deal natural join Customer where idCusto = 1")
-    array = dbCursor.fetchall()
-    
-    arrayLabelQuery = []
-    count = 0
-    for e in array:
-        arrayLabelQuery.append(tk.Label(window, text = e))
-        arrayLabelQuery[count].grid()
-        count += 1
-    
-    dbCursor.close()
+    buttonQuery = tk.Button(window, text = 'test', command = lambda : ExecuteQuery("select idCar, firstNameCusto from deal natural join Customer where idCusto = 1", window))
+    buttonQuery.grid()
 
     window.mainloop()
 
