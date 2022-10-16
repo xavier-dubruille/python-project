7f9e8b77a9a689f8df61f6276aa71592c259f57c
Samaze123456 <he202053@students.ephec.be>
2022-10-16 11:42:16 +0200

    dazda

--- Main/main.py
+++ Main/main.py
@@ -84,6 +84,7 @@ class Application:
 
         scrollbar = Scrollbar(self.frameDisplay)
         scrollbar.pack(side="right", fill="y")
+        
         carList = Car.CarListStock()
         spaceBrand = len(max(carList, key=lambda car:len(car.nameBrand)).nameBrand) + 4
         spaceType = len(max(carList, key=lambda x:len(x.nameType)).nameType) + 4
@@ -110,7 +111,7 @@ class Application:
         #     widget.destroy()
         car = Car.CarListStock()[event.widget.curselection()[0]]
         self.printDetails = f"Brand : {car.nameBrand}\nType : {car.nameType}\nMotor : {car.nameMotor}\nPrice : {car.priceCar}€\nPromo : {car.promoCar}%\nIn stock since : {car.dateStockCar}\nNext control : {car.dateTechControlCar}"
-        self.labelDetails.configure( text = self.printDetails)
+        self.labelDetails.configure(text = self.printDetails)
 
 
     # It will show you which car you sell.
